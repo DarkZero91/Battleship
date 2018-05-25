5886c2802bd310913be881fa18813f9fb3b6b1ca
Ebombo2 <emilettele@hotmail.com>
2018-05-25 18:11:39 +0200

    Introduced player-class ----------- Working: - 2 boards - 1 ship on each board
    - Check if won
    - Print status + hit_grid

--- battleship/main.py
+++ battleship/main.py
@@ -1,23 +1,17 @@
 from battleship.player import *
 
 p1 = Player()
-p1.place_cruiser(0, 0, 0)
-
 p2 = Player()
-p2.place_cruiser(1, 2, 0)
 
-# p1.visualize_board()
+p2.place_ship(Cruiser(), 0, 0, 0) # place_ship(type, x, y, orientation)
+p2.place_ship(Cruiser(), 1, 2, 0)
+
+# p2.visualize_board()
 p2.print_board()
 
 p1.make_guess(p2, 0, 0)
 p1.make_guess(p2, 0, 1)
 p1.make_guess(p2, 1, 2)
 
-# TO TEST THE WHOLE BOARD
-# for i in range(b.width):
-#     for j in range(b.height):
-#         b2.hit_grid[j][i] = b2.ship_at_location(b, i, j)
-
-if not p1.check_won():
-    p1.print_enemyhitgrid(p2)
-    p1.print_status()
+p1.print_enemyhitgrid(p2)
+p1.print_status(p2)
--- battleship/player.py
+++ battleship/player.py
@@ -11,57 +11,19 @@ class Player(object):
         self.placedships = []
         self.sunkenships = []
 
-#------------------------------------------------
-
-    def place_carrier(self, x, y, orientation):
-        ship = Carrier()
+    def place_ship(self, ship, x, y, orientation):
         self.placedships.append(self.board.place_ship(ship, x, y, orientation))
-
-    def place_battleship(self, x, y, orientation):
-        ship = Battleship()
-        self.placedships.append(self.board.place_ship(ship, x, y, orientation))
-
-    def place_cruiser(self, x, y, orientation):
-        ship = Cruiser()
-        self.placedships.append(self.board.place_ship(ship, x, y, orientation))
-
-    def place_submarine(self, x, y, orientation):
-        ship = Submarine()
-        self.placedships.append(self.board.place_ship(ship, x, y, orientation))
-
-    def place_destroyer(self, x, y, orientation):
-        ship = Destroyer()
-        self.placedships.append(self.board.place_ship(ship, x, y, orientation))
-
-#--------------------------------------------------
 
     def make_guess(self, player, x, y):
         ship = player.board.ship_at_location(x, y)
         if ship != 0:
             sunk = self.board.hit_ship(ship)
             if sunk != 0:
-                self.sunkenship.append(sunk)
-    #     ship = self.
-    #
-    # for i in range(len(self.ships)):
-    #     if self.ships[i] == ship:
-    #         self.ships.pop(i)
-    #         if self.ships == []:
-    #             self.finish = True
-    #         break
+                self.sunkenships.append(sunk)
+                self._update_ship_lists(player)
 
-    def update_ship_lists(self):
-        self.remainships = list(set(self.placedships)-set(self.sunkenships))
-        if self.remainships == []:
-            self.finish = True
-
-    def check_won(self):
-        if (self.finish):
-            print "Gewonnen!"
-            return True
-
-    def print_status(self):
-        self.update_ship_lists()
+    def print_status(self, enemy):
+        self._update_ship_lists(enemy)
         print("\nStatus:\nShips:")
         print(self.remainships)
         print("Hits:")
@@ -76,3 +38,14 @@ class Player(object):
 
     def print_enemyhitgrid(self, player):
         player.board.print_hitgrid()
+
+    def check_won(self):
+        if (self.finish):
+            print("Gewonnen!")
+            return True
+
+    def _update_ship_lists(self, enemy):
+        self.remainships = list(set(enemy.placedships) - set(self.sunkenships))
+        if self.remainships == []:
+            self.finish = True
+            self.check_won()
